76598b4ddbe84dfb0b2e8834e48608ea7df0b5c1
ajaykaranam <54679942+ajaykaranam@users.noreply.github.com>
2019-08-29 19:05:45 +0200

    Create lambdaPostExample.js
    HTTPS POST Request to Power BI
    Lambda function connecting the Dynamo DB to Power BI API through

---
+++ lambdaPostExample.js
@@ -0,0 +1,30 @@
+var https = require('https');
+/***
+Lambda function to post the data to Power BI whenever the event occurs 
+***/
+exports.handler = function(event, context) {
+
+    var body=event.Records;
+    var jsonObject = JSON.stringify(event);
+    
+    // the post options
+    var optionspost = {
+        host: 'api.powerbi.com',
+        path: '/beta/'', //power bi api
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    };
+
+    var reqPost = https.request(optionspost, function(res) {
+        console.log("statusCode: ", res.statusCode);
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+        context.succeed(body);
+    });
+
+    reqPost.write(jsonObject);
+    reqPost.end();
+};
